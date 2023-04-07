f760f772c0f907bb0a4fda1a30a5ce47b7c90897
Rifat <mdrifatahmed787@gmail.com>
2023-04-07 13:03:41 +0600

    about page added

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
       },
     ],
   },
---
+++ src/pages/About/About.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import png from "../../assets/favicon.png";
+
+const About = () => {
+  //handle scrolling
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div>
+      <section className="m-4 md:m-8 dark:bg-gray-800 dark:text-gray-100">
+        <div className="flex items-center justify-center mb-8">
+          <img className="h-10 w-10" src={png} alt="Logo" />
+          <h1 className="text-4xl font-bold">About Gift Galley</h1>
+        </div>
+        <div className="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="flex flex-col items-center p-4">
+            <img className="h-10 w-10" src={png} alt="Logo" />
+            <h3 className="my-3 text-3xl font-semibold">Product</h3>
+            <div className="space-y-1 leading-tight">
+              <p>Similique quas ea veniam</p>
+              <p>Tempore quasi porro</p>
+              <p>Blanditiis aut mollitia ex</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center p-4">
+            <img className="h-10 w-10" src={png} alt="Logo" />
+            <h3 className="my-3 text-3xl font-semibold">Product</h3>
+            <div className="space-y-1 leading-tight">
+              <p>Similique quas ea veniam</p>
+              <p>Tempore quasi porro</p>
+              <p>Blanditiis aut mollitia ex</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center p-4">
+            <img className="h-10 w-10" src={png} alt="Logo" />
+            <h3 className="my-3 text-3xl font-semibold">Product</h3>
+            <div className="space-y-1 leading-tight">
+              <p>Similique quas ea veniam</p>
+              <p>Tempore quasi porro</p>
+              <p>Blanditiis aut mollitia ex</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center p-4">
+            <img className="h-10 w-10" src={png} alt="Logo" />
+            <h3 className="my-3 text-3xl font-semibold">Product</h3>
+            <div className="space-y-1 leading-tight">
+              <p>Similique quas ea veniam</p>
+              <p>Tempore quasi porro</p>
+              <p>Blanditiis aut mollitia ex</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center p-4">
+            <img className="h-10 w-10" src={png} alt="Logo" />
+            <h3 className="my-3 text-3xl font-semibold">Product</h3>
+            <div className="space-y-1 leading-tight">
+              <p>Similique quas ea veniam</p>
+              <p>Tempore quasi porro</p>
+              <p>Blanditiis aut mollitia ex</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center p-4">
+            <img className="h-10 w-10" src={png} alt="Logo" />
+            <h3 className="my-3 text-3xl font-semibold">Product</h3>
+            <div className="space-y-1 leading-tight">
+              <p>Similique quas ea veniam</p>
+              <p>Tempore quasi porro</p>
+              <p>Blanditiis aut mollitia ex</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default About;
